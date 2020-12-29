ac1e0fce32772aa77cbddb5e9bf1c4aa85449eda
kjsang <75797388+kjsang@users.noreply.github.com>
2020-12-29 14:53:11 +0900

    201229

---
+++ ME.NO.SMILE.R
@@ -0,0 +1,17 @@
+Kimjusang <- "Gradstudent"
+print(c("Kim Jusang is", Kimjusang))
+Target <- Kimjusang
+x <- if(Target == "Gradstudent"){
+  print("사람아님")} else {
+    print("사람")
+  }
+if(x == "사람"){
+  print("웃음")} else {
+    print("웃을수없음")
+  }
+
+if(#조건) (#조건이 트루일 때) else (#조건이 F일 때)
+
+rm(Kimjusang, Target, x)
+
+
---
+++ Study_201229.R
@@ -0,0 +1,10 @@
+member <- list(name='KimJusang', 
+               address='Seoul', 
+               tel='010-4729-3031',
+               age=20,
+               married=F)
+member
+member[1]
+member$name
+
+
